6726d5f94799c86cb20857f78cee504f94f16da2
Ilya <poroskyn2170@gmail.com>
2015-09-18 17:23:52 +0300

    17:23 18.09.15

--- framework/Loader.php
+++ framework/Loader.php
@@ -5,7 +5,7 @@ class Loader
 {
     const DS = DIRECTORY_SEPARATOR;
 
-    static public $namespacePath = [];
+    static public $namespacePaths = [];
 
     private $root;
 
@@ -25,19 +25,47 @@ class Loader
             include $filePath;
         else
         {
-            list($alias) = explode('\\', $className);
-            
+            $aliases = explode('\\', $className);
+            $namespace = array_shift($aliases).'\\';
 
+            if(array_key_exists($namespace, self::$namespacePaths))
+            {
+                $residue = implode('\\', $aliases);
+                $path = self::$namespacePaths[$namespace].self::DS.$residue.'.php';
+                $filePath = str_replace('\\', '/', $path);
+                if(file_exists($filePath))
+                    include $filePath;
+
+            }
+            /*
+            $aliases = explode('\\', $className);
+            $class = array_pop($aliases);
+            $namespace = implode('\\', $aliases);
+
+            if(strripos('\\', $namespace) === false)
+                $namespace .= '\\';
+
+            echo $namespace;
+
+            if(in_array($namespace, self::$namespacePaths))
+            {
+                $path = self::$namespacePaths[$namespace].self::DS.$class.'.php';
+                $filePath = str_replace('\\', '/', $path);
+                //echo $filePath;
+                if(file_exists($filePath))
+                    include $filePath;
+
+            }
+            */
         }
 
     }
 
     static public function addNamespacePath($alias, $path)
     {
-        self::$namespacePath[$alias] = $path;
-        print_r(self::$namespacePath);
+        self::$namespacePaths[$alias] = $path;
+        //print_r(self::$namespacePaths);
     }
-
 
 }
 new Loader();
